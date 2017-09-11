61ed79e0d4824b5b3cb18b5a70356cca1b5f76b8
Xixiang Wu <xixiangwu0108@gmail.com>
2017-09-11 18:11:19 +1000

    a simple login + register system
    The game can login now!!!

---
+++ The Runner/Temp/il2cppOutput/il2cppOutput/Il2CppCompilerCalculateTypeValues_17Table.cpp
@@ -0,0 +1,197 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E_1568637717.h"
+#include "UnityEngine_Analytics_U3CModuleU3E3783534214.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Analyt2191537572.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Analyt1068911718.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Tracka1304606600.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Tracka2256174789.h"
+#include "AssemblyU2DCSharp_U3CModuleU3E3783534214.h"
+#include "AssemblyU2DCSharp_AR2523137185.h"
+#include "AssemblyU2DCSharp_GPS3691620964.h"
+#include "AssemblyU2DCSharp_GPS_U3CStartLocationServiceU3Ec__560764166.h"
+#include "AssemblyU2DCSharp_Map4262919222.h"
+#include "AssemblyU2DCSharp_Map_mapType3017953257.h"
+#include "AssemblyU2DCSharp_Map_U3CGetGoogleMapU3Ec__Iterato1695079217.h"
+#include "AssemblyU2DCSharp_UpdateGPS2150053955.h"
+#include "AssemblyU2DCSharp_LoginGUI4073642432.h"
+#include "AssemblyU2DCSharp_LoginGUI_U3CLoginU3Ec__Iterator01563009217.h"
+#include "AssemblyU2DCSharp_LoginGUI_U3CCreateAccountU3Ec__I4116917652.h"
+#include "AssemblyU2DCSharp_LoginGUI_U3CloadGameU3Ec__Iterato431962752.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1700 = { sizeof (U24ArrayTypeU3D12_t1568637717)+ sizeof (Il2CppObject), sizeof(U24ArrayTypeU3D12_t1568637717 ), 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1701 = { sizeof (U3CModuleU3E_t3783534220), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1702 = { sizeof (AnalyticsTracker_t2191537572), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1702[5] = 
+{
+	AnalyticsTracker_t2191537572::get_offset_of_m_EventName_2(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_Dict_3(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_PrevDictHash_4(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_TrackableProperty_5(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_Trigger_6(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1703 = { sizeof (Trigger_t1068911718)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1703[8] = 
+{
+	Trigger_t1068911718::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1704 = { sizeof (TrackableProperty_t1304606600), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1704[2] = 
+{
+	0,
+	TrackableProperty_t1304606600::get_offset_of_m_Fields_1(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1705 = { sizeof (FieldWithTarget_t2256174789), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1705[6] = 
+{
+	FieldWithTarget_t2256174789::get_offset_of_m_ParamName_0(),
+	FieldWithTarget_t2256174789::get_offset_of_m_Target_1(),
+	FieldWithTarget_t2256174789::get_offset_of_m_FieldPath_2(),
+	FieldWithTarget_t2256174789::get_offset_of_m_TypeString_3(),
+	FieldWithTarget_t2256174789::get_offset_of_m_DoStatic_4(),
+	FieldWithTarget_t2256174789::get_offset_of_m_StaticString_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1706 = { sizeof (U3CModuleU3E_t3783534221), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1707 = { sizeof (AR_t2523137185), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1707[7] = 
+{
+	AR_t2523137185::get_offset_of_gyro_2(),
+	AR_t2523137185::get_offset_of_cameraContainer_3(),
+	AR_t2523137185::get_offset_of_gyroRotation_4(),
+	AR_t2523137185::get_offset_of_cam_5(),
+	AR_t2523137185::get_offset_of_background_6(),
+	AR_t2523137185::get_offset_of_fit_7(),
+	AR_t2523137185::get_offset_of_arReady_8(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1708 = { sizeof (GPS_t3691620964), -1, sizeof(GPS_t3691620964_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1708[3] = 
+{
+	GPS_t3691620964_StaticFields::get_offset_of_U3CInstanceU3Ek__BackingField_2(),
+	GPS_t3691620964::get_offset_of_latitude_3(),
+	GPS_t3691620964::get_offset_of_longitude_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1709 = { sizeof (U3CStartLocationServiceU3Ec__Iterator0_t560764166), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1709[5] = 
+{
+	U3CStartLocationServiceU3Ec__Iterator0_t560764166::get_offset_of_U3CmaxWaitU3E__0_0(),
+	U3CStartLocationServiceU3Ec__Iterator0_t560764166::get_offset_of_U24this_1(),
+	U3CStartLocationServiceU3Ec__Iterator0_t560764166::get_offset_of_U24current_2(),
+	U3CStartLocationServiceU3Ec__Iterator0_t560764166::get_offset_of_U24disposing_3(),
+	U3CStartLocationServiceU3Ec__Iterator0_t560764166::get_offset_of_U24PC_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1710 = { sizeof (Map_t4262919222), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1710[12] = 
+{
+	Map_t4262919222::get_offset_of_url_2(),
+	Map_t4262919222::get_offset_of_latitude_3(),
+	Map_t4262919222::get_offset_of_longitude_4(),
+	Map_t4262919222::get_offset_of_zoom_5(),
+	Map_t4262919222::get_offset_of_mapWidth_6(),
+	Map_t4262919222::get_offset_of_mapHeight_7(),
+	Map_t4262919222::get_offset_of_mapSelected_8(),
+	Map_t4262919222::get_offset_of_myMap_9(),
+	Map_t4262919222::get_offset_of_coordinate_10(),
+	Map_t4262919222::get_offset_of_mapCoroutine_11(),
+	Map_t4262919222::get_offset_of_nextActionTime_12(),
+	Map_t4262919222::get_offset_of_period_13(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1711 = { sizeof (mapType_t3017953257)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1711[5] = 
+{
+	mapType_t3017953257::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1712 = { sizeof (U3CGetGoogleMapU3Ec__Iterator0_t1695079217), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1712[7] = 
+{
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_latitude_0(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_longitude_1(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_U3CwwwU3E__0_2(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_U24this_3(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_U24current_4(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_U24disposing_5(),
+	U3CGetGoogleMapU3Ec__Iterator0_t1695079217::get_offset_of_U24PC_6(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1713 = { sizeof (UpdateGPS_t2150053955), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1713[1] = 
+{
+	UpdateGPS_t2150053955::get_offset_of_coordinate_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1714 = { sizeof (LoginGUI_t4073642432), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1714[9] = 
+{
+	LoginGUI_t4073642432::get_offset_of_Username_2(),
+	LoginGUI_t4073642432::get_offset_of_Password_3(),
+	LoginGUI_t4073642432::get_offset_of_CreateUsername_4(),
+	LoginGUI_t4073642432::get_offset_of_CreatePassword_5(),
+	LoginGUI_t4073642432::get_offset_of_ConfirmPassword_6(),
+	LoginGUI_t4073642432::get_offset_of_PlayerEmail_7(),
+	LoginGUI_t4073642432::get_offset_of_CurrentGameMenu_8(),
+	LoginGUI_t4073642432::get_offset_of_MenuText_9(),
+	LoginGUI_t4073642432::get_offset_of_SecondMenuText_10(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1715 = { sizeof (U3CLoginU3Ec__Iterator0_t1563009217), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1715[5] = 
+{
+	U3CLoginU3Ec__Iterator0_t1563009217::get_offset_of__w_0(),
+	U3CLoginU3Ec__Iterator0_t1563009217::get_offset_of_U24this_1(),
+	U3CLoginU3Ec__Iterator0_t1563009217::get_offset_of_U24current_2(),
+	U3CLoginU3Ec__Iterator0_t1563009217::get_offset_of_U24disposing_3(),
+	U3CLoginU3Ec__Iterator0_t1563009217::get_offset_of_U24PC_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1716 = { sizeof (U3CCreateAccountU3Ec__Iterator1_t4116917652), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1716[5] = 
+{
+	U3CCreateAccountU3Ec__Iterator1_t4116917652::get_offset_of__w_0(),
+	U3CCreateAccountU3Ec__Iterator1_t4116917652::get_offset_of_U24this_1(),
+	U3CCreateAccountU3Ec__Iterator1_t4116917652::get_offset_of_U24current_2(),
+	U3CCreateAccountU3Ec__Iterator1_t4116917652::get_offset_of_U24disposing_3(),
+	U3CCreateAccountU3Ec__Iterator1_t4116917652::get_offset_of_U24PC_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1717 = { sizeof (U3CloadGameU3Ec__Iterator2_t431962752), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1717[4] = 
+{
+	U3CloadGameU3Ec__Iterator2_t431962752::get_offset_of_U3CaU3E__0_0(),
+	U3CloadGameU3Ec__Iterator2_t431962752::get_offset_of_U24current_1(),
+	U3CloadGameU3Ec__Iterator2_t431962752::get_offset_of_U24disposing_2(),
+	U3CloadGameU3Ec__Iterator2_t431962752::get_offset_of_U24PC_3(),
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
